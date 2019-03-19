40ea3fc80ff186315dff250028734c307f91818f
c0dexter <michal.dobrowolski1@gmail.com>
2019-03-20 00:47:07 +0100

    Refactor: Project has been migrated to AndroidX, dependencies have been updated to the newest versions

--- app/src/main/java/pl/c0dexter/gitlooker/MainActivity.java
+++ app/src/main/java/pl/c0dexter/gitlooker/MainActivity.java
@@ -1,6 +1,6 @@
 package pl.c0dexter.gitlooker;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
