270680a799a20d4e057c2508b19c2d54b3b9c8eb
c0dexter <michal.dobrowolski1@gmail.com>
2019-03-19 23:22:14 +0100

    Initial commit

---
+++ app/src/main/java/pl/c0dexter/gitlooker/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.c0dexter.gitlooker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
